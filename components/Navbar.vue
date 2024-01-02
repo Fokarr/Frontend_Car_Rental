5d02568a7279cb4a6fc78e30143595b7d0b01c78
Sergej Moor <sergej.moor@stud.th-luebeck.de>
2024-01-02 23:44:44 +0100

    made navbar responsive

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,11 +1,15 @@
 <template>
-    <nav class="flex items-center justify-between py-8 border-b border-gray-200 px-12 gap-6">
-        <span class="text-primary-500 basis-2/12 text-3xl font-bold tracking-[-1px]">DM</span>
-        <Search />
-        <Like />
-    </nav>
+  <nav class="py-8 border-b border-gray-200 px-6 gap-6">
+    <div class="flex items-center justify-between mb-4 gap-x-4">
+      <span class="text-primary-500 basis-2/12 text-3xl font-bold tracking-[-1px]"
+        >DM</span
+      >
+
+      <Search class="hidden sm:block" />
+      <Like />
+    </div>
+    <Search class="sm:hidden" />
+  </nav>
 </template>
 
-<script setup>
-
-</script>
+<script setup></script>
