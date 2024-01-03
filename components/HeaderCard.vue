8fb9730ac3494e6a0734cb692e027e3998d3f88c
Sergej Moor <sergej.moor@stud.th-luebeck.de>
2024-01-03 07:00:07 +0100

    updated car detail page for api calls

--- components/HeaderCard.vue
+++ components/HeaderCard.vue
@@ -1,24 +1,26 @@
 <template>
   <div
-    class="bg-information-500 rounded-lg shadow-md p-4 relative flex flex-col gap-4 max-w sm:max-w-sm lg:max-w-2xl lg:p-8"
+    class="rounded-lg shadow-md p-4 relative flex flex-col gap-4 max-w sm:max-w-sm lg:max-w-2xl lg:p-8"
+    :class="darkBlueBG ? 'bg-primary-500' : 'bg-information-500'"
   >
     <h3 class="text-white font-semibold lg:text-2xl lg:w-1/2">
-      The Best Platform for Car Rental
+      {{ heading }}
     </h3>
     <div class="text-white text-sm w-4/5 lg:w-1/2">
-      Ease of doing a car rental safely and reliably. Of course at a low price.
+      {{ subheading }}
     </div>
-    <div class="my-4 lg:mb-28">
+    <div class="my-4" :class="heading.length === 0 ? 'm-0' : 'lg:mb-28'">
       <button
         v-if="showCta"
-        class="bg-primary-500 rounded-md p-4 px-8 text-primary-0 font-base"
+        class="rounded-md p-4 px-8 text-primary-0 font-base"
+        :class="darkBlueBG ? 'bg-information-500' : 'bg-primary-500'"
       >
         Rental Now
       </button>
     </div>
     <img
-      src="/placeholder.png"
-      class="w-2/3 mx-auto lg:absolute lg:bottom-0 lg:left-0 lg:right-0 lg:w-1/2"
+      :src="carImageUrl"
+      class="mx-auto lg:absolute lg:bottom-0 lg:left-0 lg:right-0"
       alt=""
     />
   </div>
@@ -31,6 +33,22 @@ const props = defineProps({
   showCta: {
     type: Boolean,
     default: true,
+  },
+  carImageUrl: {
+    type: String,
+    default: "/placeholder.png",
+  },
+  darkBlueBG: {
+    type: Boolean,
+    default: false,
+  },
+  heading: {
+    type: String,
+    default: "The Best Platform for Car Rental",
+  },
+  subheading: {
+    type: String,
+    default: "Ease of doing a car rental safely and reliably. Of course at a low price.",
   },
 });
 </script>
---
+++ pages/car/[id].vue
@@ -0,0 +1,148 @@
+<template>
+  <div>
+    <div class="md:flex md:gap-x-8">
+      <!-- gallery -->
+      <div class="">
+        <HeaderCard
+          :showCta="false"
+          :carImageUrl="car.img"
+          :darkBlueBG="true"
+          heading="Sports car with the best design and acceleration"
+          subheading="Safety and comfort while driving a futuristic and elegant sports car"
+          class="sm:max-w-full"
+        ></HeaderCard>
+
+        <div class="flex w-full mt-4 gap-x-4 max-w md:max-w-sm lg:max-w-2xl">
+          <div class="flex-1">
+            <HeaderCard
+              :showCta="false"
+              :carImageUrl="car.img"
+              :darkBlueBG="true"
+              heading=""
+              subheading=""
+              class=""
+            ></HeaderCard>
+          </div>
+
+          <div v-for="image in car.images" :key="image.id" class="flex-1">
+            <img :src="image.url" class="rounded-lg h-full object-cover" alt="" />
+          </div>
+        </div>
+      </div>
+
+      <!-- car details card -->
+      <div
+        class="rounded-lg bg-primary-0 p-4 shadow-md my-12 flex flex-col justify-between md:my-0 md:w-full"
+      >
+        <div>
+          <div class="flex justify-between">
+            <div class="text-2xl font-black">{{ car.name }}</div>
+            <Icon name="mdi:heart" class="w-8 h-8"></Icon>
+          </div>
+          <div class="flex items-center text-primary-500 mb-4 mt-2">
+            <Icon name="mdi:star"></Icon>
+            <Icon name="mdi:star"></Icon>
+            <Icon name="mdi:star"></Icon>
+            <Icon name="mdi:star"></Icon>
+            <Icon name="mdi:star"></Icon>
+
+            <div class="text-secondary-300 text-sm ml-2">440+ Reviewer</div>
+          </div>
+
+          <!-- description -->
+          <p class="text-secondary-300 text-sm leading-loose">
+            {{ car.description }}
+          </p>
+
+          <!-- attributes -->
+          <div class="text-sm mb-8">
+            <div class="flex justify-between gap-x-4 my-4">
+              <div class="w-2/5 flex justify-between">
+                <span class="text-secondary-300">Type Car</span
+                ><span class="text-secondary-400">{{ car.type }}</span>
+              </div>
+              <div class="w-2/5 flex justify-between">
+                <span class="text-secondary-300">Capacity</span
+                ><span class="text-secondary-400">{{ car.capacity }} Person</span>
+              </div>
+            </div>
+
+            <div class="flex justify-between w-full my-2">
+              <div class="w-2/5 flex justify-between">
+                <span class="text-secondary-300">Steering</span
+                ><span class="text-secondary-400">{{ car.kindOfTransition }}</span>
+              </div>
+              <div class="w-2/5 flex justify-between">
+                <span class="text-secondary-300">Gasoline</span
+                ><span class="text-secondary-400">{{ car.gasolineLiter }}</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- price and cta -->
+        <div class="flex justify-between items-end">
+          <!-- car price -->
+          <div class="font-bold text-xl">
+            <div>
+              ${{ car.pricePerDay }}/
+              <span class="text-secondary-300 text-base">day</span>
+            </div>
+            <div class="line-through text-secondary-300 text-base mt-1 hidden">
+              ${{ car.pricePerDay }}
+            </div>
+          </div>
+
+          <!-- cta button -->
+          <CtaButton></CtaButton>
+        </div>
+      </div>
+    </div>
+    <!-- recommendations -->
+    <CarCategory heading="Recommendated Cars">
+      <!--       <CarCard></CarCard>
+      <CarCard></CarCard>
+      <CarCard></CarCard>
+      <CarCard></CarCard> -->
+    </CarCategory>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+const car = ref(null);
+
+car.value = {
+  name: "Volkswagen Golf",
+  type: "Hatchback",
+  description:
+    "The Volkswagen Golf is a hatchback that is known for its fuel efficiency and practicality. It is also available as a wagon. The Golf is available in a variety of trim levels, including the base S, the sporty GTI, and the luxurious Golf R.",
+  gasolineLiter: 50,
+  kindOfTransition: "Manual",
+  people: 5,
+  pricePerDay: 55,
+  id: "volkswagen-golf",
+  img: "https://dm-assignment-commonshare.koyeb.app/img/25.webp",
+  images: [
+    { url: "https://dm-assignment-commonshare.koyeb.app/img/car-image.jpg" },
+    { url: "https://dm-assignment-commonshare.koyeb.app/img/car-inside.jpg" },
+  ],
+};
+
+/* onMounted(async () => {
+  try {
+    const response = await fetch(`/api/cars/${route.params.id}`)
+    if (response.ok) {
+      car.value = await response.json()
+    } else {
+      console.error('Failed to fetch car data')
+    }
+  } catch (error) {
+    console.error('Failed to fetch car data:', error)
+  }
+}) */
+</script>
+
+<style lang="scss" scoped></style>
