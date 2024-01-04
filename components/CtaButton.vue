00f1962f4b59a42bf97e5848ed4f31a816665738
Jarmo <jarmof135@gmail.com>
2024-01-04 20:15:20 +0100

    rent now fix

--- components/CtaButton.vue
+++ components/CtaButton.vue
@@ -5,7 +5,7 @@
       class="bg-primary-500 flex transition-all duration-300 ease-in-out hover:cursor-pointer hover:shadow-lg hover:-translate-y-1 rounded-md p-3 px-6 relative text-primary-0 font-base"
       no-prefetch
     >
-      Rental Now
+      Rent Now
     </a>
   </div>
 </template>
